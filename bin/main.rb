0532c9ae9cdd1505642fc40e25287a8ca542c547
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-03-20 12:01:02 +0100

    add main interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,9 +1,41 @@
 require_relative '../lib/scrapper.rb'
 
-scrapper = Scrapper.new
+begin
+  scrapper = Scrapper.new
 
-scrapper.fetch_pages(10)
+  puts ''
+  puts 'Hello, welcome to the Scrapper.'
+  puts '-------------------------------'
+  puts ''
+  puts ''
 
-p scrapper.listings.count
+  while scrapper.category_number == 0
+    puts 'Select a Category:'
+    puts '1. téléphones à vendre'
+    puts '2. tablettes à vendre'
+    puts '3. ordinateurs portables à vendre'
+    puts 'Select a category:(1, 2 or 3)'
+    scrapper.category_number = gets.chomp.to_i
+  end
 
-scrapper.write
+  number_of_pages = 0
+
+  while number_of_pages == 0
+    puts 'How many pages do you want to srap:'
+    number_of_pages = gets.chomp.to_i
+  end
+
+  puts 'Scrapping start ...'
+  scrapper.fetch_pages(number_of_pages)
+  puts 'Scrapping finished'
+  puts "#{scrapper.listings.count} entries scrapped"
+  puts 'Do you want to save the file: (y/n)'
+  option = gets.chomp
+  if option == 'y' || option == 'Y'
+    scrapper.write
+  end
+
+
+rescue StandardError => e
+  puts "Rescued: #{e.inspect}"
+end
--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -3,21 +3,33 @@ require 'nokogiri'
 
 class Scrapper
   attr_reader :listings
+  attr_accessor :category_number
+
+  URL = 'https://www.avito.ma/fr/maroc/Z/'
+  CATEGORIES = %w[
+    t%C3%A9l%C3%A9phones-%C3%A0_vendre
+    tablettes-%C3%A0_vendre
+    ordinateurs_portables-%C3%A0_vendre
+  ]
 
   def initialize
-    @url = 'https://www.avito.ma/fr/maroc/t%C3%A9l%C3%A9phones-%C3%A0_vendre'
     @listings = []
+    @category_number = 0
   end
 
   def fetch_page(page)
-    page = Nokogiri::HTML(HTTParty.get(@url + "?o=#{page}"))
+    page = Nokogiri::HTML(HTTParty.get(URL + CATEGORIES[category_number - 1] + "?o=#{page}"))
+    raise 'The link is not working properly, check if the link did not change in the orginal website!' if page.nil? || page.empty?
+
     page.css(".item").each { |item| @listings << process_item(item) }
   end
 
   def fetch_pages(number_pages)
     return if number_pages <= 0
 
-    page = Nokogiri::HTML(HTTParty.get(@url))
+    page = Nokogiri::HTML(HTTParty.get(URL + CATEGORIES[category_number - 1]))
+    raise 'The link is not working properly, check if the link did not change in the orginal website!' if page.nil? || page.empty?
+
     item_per_page = page.css(".item").count
     number_listings = page.css("a#listing_tabs_all small").text.to_i
     all_pages = number_listings / item_per_page + 1
