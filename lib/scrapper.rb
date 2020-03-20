18db8cc02ac0b5d80a999f3f2a86e4f76c78e6a6
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-03-20 13:20:29 +0100

    refactor fetch pages

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -17,38 +17,14 @@ class Scrapper
     @category_number = 0
   end
 
-  def fetch_page(page)
-    response = HTTParty.get(URL + CATEGORIES[category_number - 1] + "?o=#{page}")
-    raise 'Error' if response.body.nil? || response.body.empty?
-
-    page = Nokogiri::HTML(response)
-    page.css(".item").each { |item| @listings << process_item(item) }
-  end
-
   def fetch_pages(number_pages)
     return if number_pages <= 0
 
-    response = HTTParty.get(URL + CATEGORIES[category_number - 1])
-    raise 'Error' if response.body.nil? || response.body.empty?
-
-    page = Nokogiri::HTML(response)
-    item_per_page = page.css(".item").count
-    number_listings = page.css("a#listing_tabs_all small").text.to_i
-    all_pages = number_listings / item_per_page + 1
-
+    all_pages = fetch_number_of_all_pages
     range = (number_pages <= all_pages ? 1..number_pages : 1..number_pages)
     range.each do |page|
       fetch_page(page)
     end
-  end
-
-  def process_item(item)
-    date = item.css('div.item-age').text.strip
-    title = item.css('div.item-info h2').text.strip
-    city = item.css('span.item-info-extra small a').text.strip
-    price = item.css('.price_value').text.strip
-
-    { :date => date, :title => title, :city => city, :price => price }
   end
 
   def write
@@ -61,5 +37,39 @@ class Scrapper
       file.write item[:price] + ","
       file.puts
     end
+  end
+
+  private
+
+  def process_item(item)
+    date = item.css('div.item-age').text.strip
+    date = format_date(date)
+    title = item.css('div.item-info h2').text.strip
+    city = item.css('span.item-info-extra small a').text.strip
+    price = item.css('.price_value').text.strip
+
+    { :date => date, :title => title, :city => city, :price => price }
+  end
+
+  def format_date(date)
+    date = "#{date[0..-6]} #{date[-5...date.length]}"
+  end
+
+  def fetch_page(page)
+    response = HTTParty.get(URL + CATEGORIES[category_number - 1] + "?o=#{page}")
+    raise 'Error' if response.body.nil? || response.body.empty?
+
+    page = Nokogiri::HTML(response.body)
+    page.css(".item").each { |item| @listings << process_item(item) }
+  end
+
+  def fetch_number_of_all_pages
+    response = HTTParty.get(URL + CATEGORIES[category_number - 1])
+    raise 'Error' if response.body.nil? || response.body.empty?
+
+    page = Nokogiri::HTML(response.body)
+    item_per_page = page.css(".item").count
+    number_listings = page.css("a#listing_tabs_all small").text.to_i
+    number_listings / item_per_page + 1
   end
 end
