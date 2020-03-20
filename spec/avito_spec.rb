7a0b826c81906fe2a767204fb4d7ecff6287a372
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-03-20 18:18:42 +0100

    add test for avito class

--- spec/avito_spec.rb
+++ spec/avito_spec.rb
@@ -1,1 +1,11 @@
+require_relative '../lib/avito.rb'
 
+RSpec.describe Avito do
+  let(:avito){ Avito.new }
+  describe '#initialize' do
+    it 'initialize all variable instance' do
+      expect(avito.listings).to eql([])
+      expect(avito.category_number).to eql(0)
+    end
+  end
+end
